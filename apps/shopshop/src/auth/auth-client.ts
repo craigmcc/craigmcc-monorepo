0d3f1c722fe1d673a0e8eefb1a1241a4e3c8cb90
Craig McClanahan <craigmcc@gmail.com>
2026-05-27 21:15:53 -0500

    Use BETTER_AUTH_URL in the better-auth client for defining where auth takes place.

--- apps/shopshop/src/auth/auth-client.ts
+++ apps/shopshop/src/auth/auth-client.ts
@@ -24,7 +24,7 @@ type AuthClientOptions = {
 type AuthClientType = ReturnType<typeof createAuthClient<AuthClientOptions>>;
 
 const authClientOptions: AuthClientOptions = {
-  baseURL: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:3000",
   basePath: "/api/auth",
   plugins: [
     customSessionClient<typeof auth>(),
